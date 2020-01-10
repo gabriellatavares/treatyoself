a5c759fb95605cede734d5dc86ec8990e19f9bab
estefanodischool <stefano@barcelonacodeschool.com>
2020-01-10 16:50:05 +0100

    minor

---
+++ README.md
@@ -0,0 +1,16 @@
+======   HOW TO RUN IT   ======
+Clone first the repo, then you ll need 2 terminals
+
+In the firts one :
+
+1) cd authentication_2020
+2) cd client
+3) npm install
+4) npm start
+
+In the other :
+
+1) cd authentication_2020
+2) cd server
+3) npm install
+4) nodemon index.js
--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,6 @@ function App() {
   const [isLoggedIn,setIsLoggedIn] = useState(false)
   const token = JSON.parse(localStorage.getItem('token'))
 
-  useEffect( () => {
-     verify_token()
-  },[verify_token])
-
   const verify_token = async () => {
    if( token === null )return setIsLoggedIn(false)
      try{
@@ -24,6 +20,10 @@ function App() {
         console.log(error)
      }
   }
+  useEffect( () => {
+     verify_token()
+  },[verify_token])
+
 
   const login  = (token) => {
      localStorage.setItem('token',JSON.stringify(token)) 
