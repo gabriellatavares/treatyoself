c7829742adb1587d7660409e3e9852430cee1022
estefanodi <estefanodi2009@gmail.com>
2020-04-28 09:53:29 +0200

    minor

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
 async function connecting(){
     try {
-        await mongoose.connect('mongodb+srv://estefanodi:ISEsVh1TiBgKtQCu@cluster0-os3ke.mongodb.net/auth?retryWrites=true&w=majoritys', { useUnifiedTopology: true , useNewUrlParser: true })
+        await mongoose.connect(`${your_mongo_atlas_url_here}`, { useUnifiedTopology: true , useNewUrlParser: true })
         console.log('Connected to the DB')
     } catch ( error ) {
         console.log('ERROR: Seems like your DB is not running, please start it up !!!');
