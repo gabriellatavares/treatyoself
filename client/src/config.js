fa85a98b6252044182a1966aff97b82cdbca7455
Gabriella Tavares <gabriellatatavares@gmail.com>
2021-02-07 21:47:40 +0000

    deploy

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,8 @@
-const URL = `http://localhost:3040`
-           
+import axios from 'axios',
+// =======  preparing to the deplyment  ========
+const URL = window.location.hostname === `localhost`
+            ? `http://localhost:3040` // 3030 should be replaced with your server port
+            : `http://157.230.23.136` // it should be replaced with actual domain during the deployment
 
 export { URL }
 
